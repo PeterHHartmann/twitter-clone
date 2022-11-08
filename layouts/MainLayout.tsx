5cbb75e9324e5305bd9465b9c3ffe5890f94f166
Peter <peter.helle.hartmann@gmail.com>
2022-11-08 17:22:01 +0100

    changing to functional components

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,13 +1,9 @@
-import type { ReactNode } from 'react';
+import type { PropsWithChildren } from 'react';
 import NavLeft from '../components/NavLeft/NavLeft';
 import NavRight from '../components/NavRight/NavRight';
 import style from '../styles/layouts/Main.module.scss';
 
-type MainLayoutProps = {
-  children: ReactNode | ReactNode[];
-};
-
-export default function Layout({ children }: MainLayoutProps) {
+export const Layout: React.FC<PropsWithChildren> = ({children}) => {
   return (
     <div className={style.app}>
       <NavLeft page='/' />
@@ -16,3 +12,5 @@ export default function Layout({ children }: MainLayoutProps) {
     </div>
   );
 }
+
+export default Layout;
