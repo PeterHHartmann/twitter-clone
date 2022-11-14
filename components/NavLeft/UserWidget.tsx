6248254c32ed0dc7beec3ffc8f0807a3dc54d6e6
Peter <peter.helle.hartmann@gmail.com>
2022-11-14 01:17:36 +0100

    logout btn display on small devices

--- components/NavLeft/UserWidget.tsx
+++ components/NavLeft/UserWidget.tsx
@@ -14,19 +14,19 @@ export const UserWidget: React.FC<{user: any}> = ({user}) => {
   }
 
   return (
-    <div className={style.container}>
+    <div className={displayLogout ? style.open : style.closed}>
       {displayLogout && (
         <div className={style.logout}>
           <button onClick={() => signOut()}>Sign out</button>
         </div>
       )}
       <div className={style.user} onClick={handleClick}>
-        <Image className={style.profile} src={default_pfp} alt='your profile picture' width={48} height={48}/>
+        <Image className={style.profile} src={default_pfp} alt='your profile picture' width={48} height={48} />
         <div className={style.info}>
           <div className={style.displayname}>{user?.displayname}</div>
           <div className={style.username}>{user?.username}</div>
         </div>
-        <Image className={style.more} src={icon} alt='' width={19} height={19} priority={true}/>
+        <Image className={style.more} src={icon} alt='' width={19} height={19} priority={true} />
       </div>
     </div>
   );
