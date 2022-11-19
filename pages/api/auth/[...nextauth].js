dd08c16e84ff9f44944ca95d011f73fb8d42dfa8
Peter <peter.helle.hartmann@gmail.com>
2022-11-19 15:04:30 +0100

    minor naming refactor

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -13,50 +13,59 @@ export const authOptions = {
       },
       async authorize(credentials, _req) {
         try {
-          const response = await fetch(
-            'http://localhost:8000/auth/signin', 
-            {
-              method: 'POST', 
-              headers: {
-                'content-type': 'application/json'
-              }, 
-              body: JSON.stringify(credentials)
-            }
-          );
+          const response = await fetch('http://localhost:8000/auth/signin', {
+            method: 'POST',
+            headers: {
+              'content-type': 'application/json',
+            },
+            body: JSON.stringify(credentials),
+          });
           if (response.status === 200) {
             const data = await response.json();
             return {
               username: data.username,
               displayname: data.displayname,
-              access_token: data.access_token,
+              accessToken: data.accessToken,
             };
           } else {
             const { error } = await response.json();
-            return Promise.reject(new Error(error))
+            return Promise.reject(new Error(error));
           }
-        } catch(e) { 
+        } catch (e) {
           return Promise.reject(new Error('Something went wrong please try again later'));
         }
       },
     }),
   ],
+  refetchInterval: 30,
+  secret: process.env.NEXTAUTH_SECRET,
   pages: {
-    signIn: "/signin"
+    signIn: '/signin',
+  },
+  // session: {
+  //   strategy: 'jwt',
+  //   maxAge: 30,
+  // },
+  jwt: {
+    maxAge: 30,
   },
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
-        token.user = {
-          username: user.username,
-          displayname: user.displayname,
-          access_token: user.access_token
-        }
+        return {
+          user,
+          accessToken: user.accessToken,
+        };
       }
       return token;
     },
     async session({ session, token }) {
-      if (token.user) {
-        session.user = token.user
+      if (token) {
+        session.user = {
+          username: token.user.username,
+          displayname: token.user.displayname,
+        };
+        session.accessToken = token.accessToken;
       }
       return session;
     },
